e0ef098910602b1b99a0dd3a56baf37620d2a69e
zerospeedzero <zerospeedzero@gmail.com>
2022-10-25 19:03:31 -0600

    Fallback to using getServerSideProps

--- pages/types/[id].js
+++ pages/types/[id].js
@@ -5,14 +5,24 @@ function type({id, darkMode}) {
     return <Type id={id} darkMode={darkMode}/>
 }
 
-export async function getStaticProps(context) {
-    const { id } = context.query
-    return {
-        props: {
-            id
-        }
-    }
-}
+// export async function getStaticPaths() {
+//     return {
+//       paths: [{ params: { id: 'innovator' } }, { params: { id: 'artist' } }, { params: { id: 'thinker' } }, { params: { id: 'adventurer' } }, 
+//       { params: { id: 'maker' } }, { params: { id: 'producer' } }, { params: { id: 'dreamer' } }, { params: { id: 'visionary' } }],
+//       fallback: false, // can also be true or 'blocking'
+//     }
+//   }
+
+// export async function getStaticProps(context) {
+//     const  id  = context.params.id
+//     console.log(context)
+//     console.log(id)
+//     return {
+//         props: { id: id},
+//     }
+// }
+
+
 export async function getServerSideProps(context) {
     const { id } = context.query
     return {
@@ -21,5 +31,6 @@ export async function getServerSideProps(context) {
         }
     }
 }
+
 
 export default type
