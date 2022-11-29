753b43bd517c8db3524cb6015eb2eeca1c76c5b8
zerospeedzero <zerospeedzero@gmail.com>
2022-11-29 00:36:42 -0700

    Fix noreferrer

--- pages/about.js
+++ pages/about.js
@@ -99,29 +99,29 @@ function about() {
                 <li>Join a SAITSA Club that interest you.</li>
                 <li>Borrow a book from the Reg Erhardt Library</li>
             </ul>
-            <p>To know more about <a href="https://www.sait.ca/student-life" target="_blank">student life</a>.</p>
+            <p>To know more about <a href="https://www.sait.ca/student-life" target="_blank" rel="noreferrer">student life</a>.</p>
             <img className={styles.img} src="studentlife2.png"/>
             <h2>SAIT education different and why SAIT</h2>
             <p>You are a rough diamond that can be polished with a high-quality education. SAIT offers you not only that but the opportunity to learn by experience and connect with other professionals in the field. NMPD’s instructors are highly qualified professionals that teach the most updated techniques in the media production industry. Additionally, you will never be alone, SAIT is always to support you to reach succeed.</p>
             <img className={styles.img} src="studentlife3.png"/>
             <h2>Funding & Scholarships</h2>
             <p>With the generous support of SAIT donors, SAIT is proud to offer over $5 million in awards to students in almost every program. Awards are available to all qualifying SAIT students in recognition of academic success, financial need, community involvement, and other areas of success and support.</p>
-            <p>You are able to find the awards in the <a target="_blank" href="https://www.sait.ca/admissions/tuition-and-financial-aid/student-awards">Student Awards Guide 2022/23</a></p>
-            <p>For more information, please visit <a target="_blank" href="https://www.sait.ca/programs-and-courses/diplomas/new-media-production-and-design" rel="noreferrer"  target="_blank">Sait NMPD website</a></p>
+            <p>You are able to find the awards in the <a target="_blank" rel="noreferrer" href="https://www.sait.ca/admissions/tuition-and-financial-aid/student-awards">Student Awards Guide 2022/23</a></p>
+            <p>For more information, please visit <a target="_blank" rel="noreferrer" href="https://www.sait.ca/programs-and-courses/diplomas/new-media-production-and-design" rel="noreferrer"  target="_blank">Sait NMPD website</a></p>
             <img className={styles.img} src="studentlife1.png"/>
             <h2>Making of this website</h2>
             <p>Six students of New Media Production and Design are working on the design, development and soft launch of this website using the following technologies.</p>
             <ul>
-                <li><a target="_target" href="https://www.maxon.net/en/cinema-4d">Maxon Cinema 4D</a> - Create 3D models and animations for this website</li>
-                <li><a target="_target" href="https://www.adobe.com/ca/products/aftereffects.html">Adobe After Effects</a> - Video and VFX production tools for this website</li>
-                <li><a target="_target" href="https://www.adobe.com/ca/products/illustrator.html">Adobe Illustrator</a> - Graphics, icons and background image production tools for this website</li>
-                <li><a target="_target" href="https://portal.azure.com">Microsoft Azure Cloud Computing</a> - Running cloud apps anywhere anytime on any cloud environments</li>
-                <li><a target="_target" href="https://github.com/vercel/next.js/">NextJs</a> - Javascript-based framework - modern web development platform</li>
-                <li><a target="_target" href="https://github.com/framer/motion">Framer motion</a> - Easy and stable UX animation experience technology</li>
-                <li><a target="_target" href="https://github.com/aralroca/next-translate">NextJs translate</a> - Supports six languages to potential NMPD students</li>
-                <li><a target="_target" href="https://github.com/Bunlong/next-share">NextJs share</a> - Share NMPD link to social media including Facebook, WhatsApp, Reddit and LinkedIn</li>
-                <li><a target="_target" href="https://github.com/danalloway/react-country-flag">NextJs country flags</a> - Provides language switching for users</li>
-                <li><a target="_target" href="https://analytics.google.com/analytics/web/">Google analytics</a> - Provides key performance index and data for measurement</li>
+                <li><a target="_target" rel="noreferrer" href="https://www.maxon.net/en/cinema-4d">Maxon Cinema 4D</a> - Create 3D models and animations for this website</li>
+                <li><a target="_target" rel="noreferrer" href="https://www.adobe.com/ca/products/aftereffects.html">Adobe After Effects</a> - Video and VFX production tools for this website</li>
+                <li><a target="_target" rel="noreferrer" href="https://www.adobe.com/ca/products/illustrator.html">Adobe Illustrator</a> - Graphics, icons and background image production tools for this website</li>
+                <li><a target="_target" rel="noreferrer" href="https://portal.azure.com">Microsoft Azure Cloud Computing</a> - Running cloud apps anywhere anytime on any cloud environments</li>
+                <li><a target="_target" rel="noreferrer" href="https://github.com/vercel/next.js/">NextJs</a> - Javascript-based framework - modern web development platform</li>
+                <li><a target="_target" rel="noreferrer" href="https://github.com/framer/motion">Framer motion</a> - Easy and stable UX animation experience technology</li>
+                <li><a target="_target" rel="noreferrer" href="https://github.com/aralroca/next-translate">NextJs translate</a> - Supports six languages to potential NMPD students</li>
+                <li><a target="_target" rel="noreferrer" href="https://github.com/Bunlong/next-share">NextJs share</a> - Share NMPD link to social media including Facebook, WhatsApp, Reddit and LinkedIn</li>
+                <li><a target="_target" rel="noreferrer" href="https://github.com/danalloway/react-country-flag">NextJs country flags</a> - Provides language switching for users</li>
+                <li><a target="_target" rel="noreferrer" href="https://analytics.google.com/analytics/web/">Google analytics</a> - Provides key performance index and data for measurement</li>
             </ul>
             <h2>Team members</h2>
             <ul>
