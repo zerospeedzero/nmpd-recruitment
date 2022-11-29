cd1333b925d44e500e60e109caa83c471c34a136
zerospeedzero <zerospeedzero@gmail.com>
2022-11-29 10:32:40 -0700

    Include GA on sait link

--- pages/about.js
+++ pages/about.js
@@ -99,15 +99,15 @@ function about() {
                 <li>Join a SAITSA Club that interest you.</li>
                 <li>Borrow a book from the Reg Erhardt Library</li>
             </ul>
-            <p>To know more about <a href="https://www.sait.ca/student-life" target="_blank" rel="noreferrer">student life</a>.</p>
+            <p>To know more about <a onclick="handleEvent2(e); return true;" href="https://www.sait.ca/student-life" target="_blank" rel="noreferrer">student life</a>.</p>
             <img className={styles.img} src="studentlife2.png"/>
             <h2>SAIT education different and why SAIT</h2>
             <p>You are a rough diamond that can be polished with a high-quality education. SAIT offers you not only that but the opportunity to learn by experience and connect with other professionals in the field. NMPD’s instructors are highly qualified professionals that teach the most updated techniques in the media production industry. Additionally, you will never be alone, SAIT is always to support you to reach succeed.</p>
             <img className={styles.img} src="studentlife3.png"/>
             <h2>Funding & Scholarships</h2>
             <p>With the generous support of SAIT donors, SAIT is proud to offer over $5 million in awards to students in almost every program. Awards are available to all qualifying SAIT students in recognition of academic success, financial need, community involvement, and other areas of success and support.</p>
-            <p>You are able to find the awards in the <a target="_blank" rel="noreferrer" href="https://www.sait.ca/admissions/tuition-and-financial-aid/student-awards">Student Awards Guide 2022/23</a></p>
-            <p>For more information, please visit <a target="_blank" rel="noreferrer" href="https://www.sait.ca/programs-and-courses/diplomas/new-media-production-and-design">Sait NMPD website</a></p>
+            <p>You are able to find the awards in the <a onclick="handleEvent2(e); return true;" target="_blank" rel="noreferrer" href="https://www.sait.ca/admissions/tuition-and-financial-aid/student-awards">Student Awards Guide 2022/23</a></p>
+            <p>For more information, please visit <a onclick="handleEvent2(e); return true;" target="_blank" rel="noreferrer" href="https://www.sait.ca/programs-and-courses/diplomas/new-media-production-and-design">Sait NMPD website</a></p>
             <img className={styles.img} src="studentlife1.png"/>
             <h2>Making of this website</h2>
             <p>Six students of New Media Production and Design are working on the design, development and soft launch of this website using the following technologies.</p>
